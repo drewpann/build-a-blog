4fe11a57cb074c83023902a58e294c6f9ac98172
Drew Pannebecker <drewpann@gmail.com>
2017-05-01 23:33:30 -0500

    Got get-by-id to function correctly

--- build-a-blog/main.py
+++ build-a-blog/main.py
@@ -29,9 +29,8 @@ class MainHandler(webapp2.RequestHandler):
         self.response.out.write("""Check out the blog <a href="/blog">here</a>!""")
 
 class Blog(Handler):
-    def render_blog(self, title="", entry=""):
+    def render_blog(self):
         recent_entries = db.GqlQuery("SELECT * FROM Entry ORDER BY created DESC LIMIT 5")
-
         self.render("view.html", entries=recent_entries)
 
     def get(self):
@@ -58,10 +57,26 @@ class NewPost(Handler):
             error = "Please enter both a title and an entry!"
             self.render_write(title, entry, error)
 
+class AllPosts(Handler):
+    def render_all(self):
+        all_entries = db.GqlQuery("SELECT * From Entry ORDER BY created DESC")
+        self.render("view.html", entries=all_entries)
+
+    def get(self):
+        self.render_all()
+
+class ViewPostHandler(Handler):
+    def render_post(self, id):
+        entry = Entry.get_by_id(int(id))
+        self.render("viewone.html", entry=entry)
+
+    def get(self,id):
+        self.render_post(5770237022568448)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/blog', Blog),
-#    ('/all', AllPosts),
-    ('/newpost', NewPost)
+    ('/all', AllPosts),
+    ('/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
