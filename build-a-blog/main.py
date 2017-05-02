2eabfae43c440e37c783ec58496ccac1ac17f1d8
Drew Pannebecker <drewpann@gmail.com>
2017-05-01 20:28:40 -0500

    Initial Functionality

--- build-a-blog/main.py
+++ build-a-blog/main.py
@@ -5,7 +5,7 @@ import jinja2
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Envrionment(loader = jinja2.FileSystemLoader(template_dir),
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
 class Handler(webapp2.RequestHandler):
@@ -19,10 +19,33 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Entry(db.Model):
+    title = db.StringProperty(required = True)
+    entry = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(Handler):
+    def render_front(self, title="", entry="", error=""):
+        entries = db.GqlQuery("SELECT * FROM Entry ORDER BY created DESC")
+
+        self.render("front.html", title=title, entry=entry, error=error, entries=entries)
+
     def get(self):
-        self.write("asciichan!")
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        entry = self.request.get("entry")
+
+        if title and entry:
+            e = Entry(title = title, entry = entry)
+            e.put()
+
+            self.redirect("/")
+        else:
+            error = "Please enter both a title and an entry!"
+            self.render_front(title, entry, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
